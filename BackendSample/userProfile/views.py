65e86532bca3759db3cc816aabeda981f6b3022e
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2025-03-22 08:38:13 +0600

    add profile section and and chage password feature

--- BackendSample/userProfile/views.py
+++ BackendSample/userProfile/views.py
@@ -13,3 +13,18 @@ class UserProfileView(APIView):
             return Response(serializer.data)
         except Profile.DoesNotExist:
             return Response({'message': 'Profile not found'}, status=404)
+
+
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        user = request.user
+        old_password = request.data.get('old_password')
+        new_password = request.data.get('new_password')
+        print('the old password is: ', old_password)
+        print('the new password is: ', new_password)
+        if not user.check_password(old_password):
+            return Response({'message': 'Old password is incorrect'}, status=400)
+        user.set_password(new_password)
+        user.save()
+        return Response({'message': 'Password changed successfully'}, status=200)
